21db8fca7dfb3844b6013e390d294c1c809edbfc
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2019-11-29 13:46:01 +0100

    refactorizacion

--- lovivido/LoVivido.java
+++ lovivido/LoVivido.java
@@ -5,33 +5,31 @@ import javax.swing.JOptionPane;
 public class LoVivido {
 
     public static void main(String[] args) {
-        String n;
-        String e;
+        String nombre;
+        String edad;
 
-        int m;
-        int d;
-        int h;
+        int meses;
+        int dias;
+        int horas;
         int a;
 
-        n = JOptionPane.showInputDialog("Escriba su nombre: ");
-        e = JOptionPane.showInputDialog("Escriba su edad: ");
+        nombre = JOptionPane.showInputDialog("Escriba su nombre: ");
+        edad = JOptionPane.showInputDialog("Escriba su edad: ");
 
-        a = Integer.parseInt(e);
+        a = Integer.parseInt(edad);
 
-        m = (a * 12);
-        d = (a * 365);
-        h = (d * 24);
+        meses = (a * 12);
+        dias = (a * 365);
+        horas = (dias * 24);
 
-        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Meses: " + meses, "Numero de meses vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
-        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Días: " + dias, "Numero de días vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
-        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Horas: " + horas, "Numero de horas vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
         System.exit(0);
-
+// en refactor puse rename para cambiar a nombres donde a n=nombre, e=edad, m=meses, d=dias, h=horas .
     }
 
 }
-
-//Aquí te lo voy a subir con el formateoo
